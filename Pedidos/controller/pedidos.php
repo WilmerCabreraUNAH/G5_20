efaf9405f9b747cba43373a5c863bd40d9990388
abelzelaya11 <abelenriquezelaya@hotmail.com>
2021-12-06 20:48:34 -0600

    actualizacion

--- Pedidos/controller/pedidos.php
+++ Pedidos/controller/pedidos.php
@@ -1,11 +1,11 @@
 <?php
-    if($_SERVER['REQUEST_METHOD'] === 'OPTIONS'){
+    if($_SERVER['REQUEST_METHOD']==='OPTIONS'){
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
         header('Access-Control-Allow-Headers: token, Content-Type');
         header('Access-Control-Max-Age: 1728000');
         header('Content-Length: 0');
-        header('Content Type: text/plain');
+        header('Content-Type: text/plain');
         die();
     }
     header('Access-Control-Allow-Origin: *');
@@ -13,12 +13,11 @@
 
     require_once("../../config/conexion.php");
     require_once("../../Pedidos/models/Pedidos.php");
-
     $pedidos = new Pedidos();
 
     $body = json_decode(file_get_contents("php://input"), true);
 
-    switch($_GET ["op"]){
+    switch($_GET["op"]){
 
         case "GetPedidos":
             $datos=$pedidos->get_pedidos();
@@ -27,10 +26,10 @@
 
         case "GetUno":
             $datos=$pedidos->get_pedido($body["ID"]);
-            echo json_encode($datos);  
+            echo json_encode($datos);
         break;
-        
-        case "InsertPedidos":
+
+        case "insertPedidos":
             $datos=$pedidos->insert_pedidos($body["ID_SOCIO"], $body["FECHA_PEDIDO"], $body["DETALLE"], $body["SUB_TOTAL"], $body["TOTAL_ISV"], $body["TOTAL"], $body["FECHA_ENTREGA"], $body["ESTADO"]);
             echo json_encode("Pedido Agregado");
         break;
--- Pedidos/models/Pedidos.php
+++ Pedidos/models/Pedidos.php
@@ -1,51 +1,60 @@
 <?php
-    
     class Pedidos extends Conectar{
 
-        public function get_pedidos(){            
-            $Conectar=parent::Conexion();
+        Public function get_pedidos(){
+            $conectar= parent::Conexion();
             parent::set_names();
-            $sql="SELECT * FROM ma_pedidos WHERE ESTADO = 1";
-            $sql=$Conectar->prepare($sql);
+            $sql="SELECT * FROM ma_pedidos";
+            $sql=$conectar->prepare($sql);
             $sql->execute();
-            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);            
         }
-
-        public function get_pedido($ID){            
-            $Conectar=parent::Conexion();
+        
+        public function get_pedido($ID){
+            $conectar= parent::Conexion();
             parent::set_names();
-            $sql="SELECT * FROM ma_pedidos WHERE ESTADO=1 AND ID = ?";
-            $sql=$Conectar->prepare($sql); 
+            $sql="SELECT * FROM ma_pedidos WHERE ID= ?";
+            $sql=$conectar-> prepare($sql);
             $sql->bindValue(1, $ID);
             $sql->execute();
             return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
         }
 
-        public function insert_pedidos($ID_SOCIO, $FECHA_PEDIDO, $DETALLE, $SUB_TOTAL, $TOTAL_ISV, $TOTAL, $FECHA_ENTREGA, $ESTADO){
-            $Conectar=parent::Conexion();
-            parent::set_names();
+        public function insert_pedidos($ID_SOCIO,$FECHA_PEDIDO,$DETALLE,$SUB_TOTAL,$TOTAL_ISV,$TOTAL,$FECHA_ENTEGA,$ESTADO)
+        {   $conectar= parent::Conexion();
+            parent :: set_names();
             $sql="INSERT INTO ma_pedidos(ID, ID_SOCIO, FECHA_PEDIDO, DETALLE, SUB_TOTAL, TOTAL_ISV, TOTAL, FECHA_ENTREGA, ESTADO)
             VALUES (NULL,?,?,?,?,?,?,?,?);";
-            $sql=$Conectar->prepare($sql);
-            $sql->bindValue(1, $ID_SOCIO);
-            $sql->bindValue(2, $FECHA_PEDIDO);
-            $sql->bindValue(3, $DETALLE);
-            $sql->bindValue(4, $SUB_TOTAL);
-            $sql->bindValue(5, $TOTAL_ISV);
-            $sql->bindValue(6, $TOTAL);
-            $sql->bindValue(7, $FECHA_ENTREGA);
-            $sql->bindValue(8, $ESTADO);
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $ID);
+            $sql->bindValue(2, $ID_SOCIO);
+            $sql->bindValue(3, $FECHA_PEDIDO);
+            $sql->bindValue(4, $DETALLE);
+            $sql->bindValue(5, $SUB_TOTAL);
+            $sql->bindValue(6, $TOTAL_ISV);
+            $sql->bindValue(7, $TOTAL);
+            $sql->bindValue(8, $FECHA_ENTREGA);
+            $sql->bindValue(9, $ESTADO);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+
+        public function delete_pedidos($ID){
+            $conectar= parent::Conexion();
+            parent::set_names();
+            $sql="DELETE FROM ma_pedidos WHERE ID= ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $ID);
             $sql->execute();
             return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
         }
 
-        public function update_pedidos($ID, $ID_SOCIO, $FECHA_PEDIDO, $DETALLE, $SUB_TOTAL, $TOTAL_ISV, $TOTAL, $FECHA_ENTREGA, $ESTADO){            
-            $Conectar=parent::Conexion();
+        public function update_pedidos($ID, $ID_SOCIO, $FECHA_PEDIDO, $DETALLE, $SUB_TOTAL, $TOTAL_ISV, $TOTAL, $FECHA_ENTREGA, $ESTADO){
+            $conectar= parent::Conexion();
             parent::set_names();
-            $sql="UPDATE ma_pedidos
-                SET ID_SOCIO=?, FECHA_PEDIDO=?, DETALLE=?, SUB_TOTAL=?, TOTAL_ISV=?, TOTAL=?, FECHA_ENTREGA=?, ESTADO=?
-                WHERE ID = ?";
-            $sql=$Conectar->prepare($sql);
+            $sql="UPDATE ma_pedidos SET ID_SOCIO=?, FECHA_PEDIDO=?, DETALLE=?, SUB_TOTAL=?, TOTAL_ISV=?, TOTAL=?, FECHA_ENTREGA=?, ESTADO=? WHERE ID = ?";
+            $sql=$conectar->prepare($sql);
             $sql->bindValue(1, $ID_SOCIO);
             $sql->bindValue(2, $FECHA_PEDIDO);
             $sql->bindValue(3, $DETALLE);
@@ -57,17 +66,7 @@
             $sql->bindValue(9, $ID);
             $sql->execute();
             return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
-        }      
-
-        public function delete_pedidos($ID){            
-            $Conectar=parent::Conexion();
-            parent::set_names();
-            $sql="DELETE FROM ma_pedidos WHERE ID=?";
-            $sql=$Conectar->prepare($sql);
-            $sql->bindValue(1, $ID);
-            $sql->execute();
-            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
         }
-
     }
+?>
 ?>
